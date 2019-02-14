1026ff3e6120941dbc36cd7b080bc690b23df7d5
sy <yisui@skspruce.net>
2019-02-14 16:37:50 +0800

    ui熟悉

---
+++ src/app/tab1-component/input-textarea/input-textarea.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-textarea',
+  templateUrl: './input-textarea.component.html',
+  styleUrls: ['./input-textarea.component.scss']
+})
+export class InputTextareaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/tab1-component/tab1-component.module.ts
+++ src/app/tab1-component/tab1-component.module.ts
@@ -1,15 +1,50 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { ActionSheetComponent } from './action-sheet/action-sheet.component';
 import { AlertComponent } from './alert/alert.component';
+import { IonicModule } from '@ionic/angular';
+import { BadgeComponent } from './badge/badge.component';
+import { ButtonComponent } from './button/button.component';
+import { CheckboxComponent } from './checkbox/checkbox.component';
+import { ChipComponent } from './chip/chip.component';
+import { DateTimeComponent } from './date-time/date-time.component';
+import { FloatActionButtonComponent } from './float-action-button/float-action-button.component';
+import { GridComponent } from './grid/grid.component';
+import { InfiniteScrollComponent } from './infinite-scroll/infinite-scroll.component';
+import { InputTextareaComponent } from './input-textarea/input-textarea.component';
 
 @NgModule({
+    imports: [
+        IonicModule,
+        CommonModule,
+        FormsModule
+    ],
     declarations: [
         ActionSheetComponent,
-        AlertComponent
+        AlertComponent,
+        BadgeComponent,
+        ButtonComponent,
+        CheckboxComponent,
+        ChipComponent,
+        DateTimeComponent,
+        FloatActionButtonComponent,
+        GridComponent,
+        InfiniteScrollComponent,
+        InputTextareaComponent
     ],
-    imports: [
-        CommonModule
+    exports: [
+        ActionSheetComponent,
+        AlertComponent,
+        BadgeComponent,
+        ButtonComponent,
+        CheckboxComponent,
+        ChipComponent,
+        DateTimeComponent,
+        FloatActionButtonComponent,
+        GridComponent,
+        InfiniteScrollComponent,
+        InputTextareaComponent
     ]
 })
 export class Tab1ComponentModule { }
